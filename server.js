ffbfc3327359a60d601ffd157615ff4826fdb638
Alex Allen <purosurbxo@gmail.com>
2024-05-14 21:10:38 -0700

    test1

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ var router = express.Router;
 // Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("we're connected!");
-});
-
 //Schema n Model
 var urlSchema = new mongoose.Schema({
   id: Number,
