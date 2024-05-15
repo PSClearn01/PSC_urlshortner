57bcfa5142248b4b20419e2c5d1d1113f3cf9baf
Alex Allen <purosurbxo@gmail.com>
2024-05-14 20:53:34 -0700

    added db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var router = express.Router;
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
